a1e085dcce78b11a34796d972d0cc9ca94adb379
nikhilkumbhare <nikhilk190697@gmail.com>
2021-03-11 16:47:16 +0530

    Added: To Create Linked List By Appending Data

--- src/main/java/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -21,4 +21,30 @@ public class MyLinkedList {
             this.head.setNext(tempNode);
         }
     }
+
+    public void append(INode newNode) {
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        if (this.tail == null) {
+            this.tail = newNode;
+        } else {
+            INode tempNode = this.tail;
+            this.tail = newNode;
+            tempNode.setNext(newNode);
+        }
+    }
+
+    public void printMyNodes() {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null) {
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
 }
--- src/main/java/com/bridgelabz/linkedlist/MyNode.java
+++ src/main/java/com/bridgelabz/linkedlist/MyNode.java
@@ -24,6 +24,7 @@ public class MyNode<K> implements INode<K> {
     }
 
     public void setNext(INode next) {
+
         this.next = (MyNode<K>) next;
     }
 }
