45ae6ee601d7bd19f3aefbed2b6ef831d91b7ef2
nikhilkumbhare <nikhilk190697@gmail.com>
2021-03-11 22:56:04 +0530

    Added: Ability To Delete Last Element In Linked List

--- src/test/java/MyNodeTest.java
+++ src/test/java/MyNodeTest.java
@@ -80,4 +80,23 @@ public class MyNodeTest {
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3Number_WhenDeletedLastElement_ShouldPassTheLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myThirdNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myFirstNode);
+        myLinkedList.printMyNodes();
+
+        myLinkedList.popLast();
+
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.tail.equals(mySecondNode);
+        Assert.assertTrue(result);
+    }
 }
